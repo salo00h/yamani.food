f1b4be7f705efdefb4417de8f22ffdbe036994dc
Saleh BA-ZIGHIFAN <saleh738297@gmail.com>
2026-04-25 14:15:12 +0200

    improve delivery restriction message

--- script.js
+++ script.js
@@ -1267,8 +1267,6 @@ confirmOrderBtn.addEventListener("click", () => {
 
     if (zipValue !== ALLOWED_DELIVERY_ZIP){
       checkoutAlert.textContent =
-        if (zipValue !== ALLOWED_DELIVERY_ZIP){
-  checkoutAlert.textContent =
        "🚫 La livraison est disponible uniquement à Lyon 8 (69008).\n\n✨ Pas de souci !\nVous pouvez choisir le retrait à emporter ou planifier votre commande à l’avance 😊";
       checkoutAlert.classList.add("show");
       return;
